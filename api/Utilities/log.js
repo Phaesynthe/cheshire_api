86dc64f2ad51112c0b5a28017a87f485047db1ad
mwj8410 <mwj8410@gmail.com>
2017-03-26 22:34:26 -0500

    fix(log): correct typo

--- api/Utilities/log.js
+++ api/Utilities/log.js
@@ -4,7 +4,7 @@ require('colors');
 
 module.exports = {
   error: (module, method, message, error) => {
-    process.stdout.writ(`${module.red} ${method}: ${message}\n ${error}\n`);
+    process.stdout.write(`${module.red} ${method}: ${message}\n ${error}\n`);
   },
   info: (module, method, message) => {
     process.stdout.write(`${('Information: ' + module + ' >> ' + method).cyan}: ${message}\n`);
